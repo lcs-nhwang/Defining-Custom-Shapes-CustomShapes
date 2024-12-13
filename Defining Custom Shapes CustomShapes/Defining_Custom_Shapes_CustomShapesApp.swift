adc626ed4ba07a6660398748aa87cdf1604362e9
Kevin Chen <kchen@lcs.on.ca>
2024-12-13 14:47:52 -0500

    Initial Commit

---
+++ Defining Custom Shapes CustomShapes/Defining_Custom_Shapes_CustomShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Defining_Custom_Shapes_CustomShapesApp.swift
+//  Defining Custom Shapes CustomShapes
+//
+//  Created by Haowen Chen on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct Defining_Custom_Shapes_CustomShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
